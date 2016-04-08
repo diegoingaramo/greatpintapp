6a1013d818d17006a18b999397172e2ba728c561
diegoingaramo <diegoingaramo@gmail.com>
2016-04-08 00:10:25 -0300

    signup menu added

--- public/js/directives/navbar/navbar.js
+++ public/js/directives/navbar/navbar.js
@@ -1,6 +1,22 @@
 app.directive('customnavbar', function() {
+    
+    var controller = ['$scope', function ($scope) {
+        
+        //Model Initialization
+        $scope.showSignin = 0;
+        $scope.notFoundUser = false;
+        $scope.userAlreadyExist = false;
+        
+        $scope.toggleSignin = function(){
+           $scope.showSignin = ($scope.showSignin == 0)?1:0;  
+        };
+    
+        
+    }];
+    
     return {
         restrict : 'E',
-        templateUrl: 'js/directives/navbar/navbar.html'
+        templateUrl: 'js/directives/navbar/navbar.html',
+        controller: controller
     }
 });
